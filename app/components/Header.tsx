ecc5aae4918f8432d6706f536b2ce02565117a45
Inma Sanchez <isanmenarguez@gmail.com>
2026-02-16 20:32:25 +0100

    feat: add mobile-first Header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,8 @@
+import Logo from './Logo';
+export default function Header() {
+  return (
+    <header className="w-full bg-neutral-900 px-3 py-2 flex justify-center">
+      <Logo />
+    </header>
+  );
+}
